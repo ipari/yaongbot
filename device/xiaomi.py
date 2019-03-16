cabcc654d618ad78aa1e9d091385ff51bf88f2a9
ipari <me@ipari.dev>
2019-03-16 17:27:56 +0900

    add discover decorator to MiAir methods

--- device/xiaomi.py
+++ device/xiaomi.py
@@ -1,27 +1,46 @@
+import time
 from miio.airpurifier import AirPurifier, OperationMode
 
 
 __all__ = ['MiAir']
 
 
+TTL = 300
+
+
+def discover_required(func):
+    def wrapper(self, *args, **kwargs):
+        now = time.time()
+        if now - self.last_discovered > TTL:
+            self.device.discover()
+            self.last_discovered = now
+        return func(self, *args, **kwargs)
+    return wrapper
+
+
 class MiAir(object):
     def __init__(self, ip, token):
         self.device = AirPurifier(ip=ip, token=token)
+        self.last_discovered = 0
 
     @property
+    @discover_required
     def status(self):
-        self.device.discover()
         return self.device.status()
 
+    @discover_required
     def be_custom(self, level):
         self.device.set_favorite_level(level)
         self.device.set_mode(OperationMode.Favorite)
 
+    @discover_required
     def be_silent(self):
         self.device.set_mode(OperationMode.Silent)
 
+    @discover_required
     def be_auto(self):
         self.device.set_mode(OperationMode.Auto)
 
+    @discover_required
     def be_turbo(self):
         self.be_custom(16)
